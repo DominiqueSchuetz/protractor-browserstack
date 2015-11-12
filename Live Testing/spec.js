e6376e29e30212916d91b768a20519df8c5db858
Domi <schuetz.dominique@gmail.com>
2015-11-12 14:48:25 +0100

    Screenshot 1. Versuch

--- Live Testing/spec.js
+++ Live Testing/spec.js
@@ -1,3 +1,16 @@
+var fs = require('fs');
+
+
+// abstract writing screen shot to a file
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
+
+
 describe('search', function() {
   it('should search a keyword on google', function() {
     browser.driver
@@ -7,7 +20,12 @@ describe('search', function() {
           .findElement(by.name('q'))
           .sendKeys('BrowserStack\n');
         browser.sleep(5000);
-        browser.takeScreenshot();
+        
+        //browser.takeScreenshot();
+        
+		browser.takeScreenshot().then(function (png) {
+			writeScreenShot(png, '/Users/dominique/Documents/GitProjetFolder/protractor-browserstack/Live Testing/Screenshots/exception.png');
+		});
       });
   });
 });
